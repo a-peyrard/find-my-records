285c210852366e4c648bf8278c7603a57857a38a
Augustin Peyrard <augustin.peyrard@gmail.com>
2017-06-26 18:48:07 -0700

    minor: removes `greeter` samples

--- src/__tests__/greeter.ts
+++
@@ -1,8 +0,0 @@
-import {} from "jest";
-import greeter from "../greeter";
-
-describe("greeter", () => {
-    it("should greet the person", () => {
-        expect(greeter("Augustin")).toBe("Hello, Augustin");
-    });
-});
